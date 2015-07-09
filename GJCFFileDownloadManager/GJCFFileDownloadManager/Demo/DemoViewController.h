9182b3fba8657db351ed1585b8fbeb8737a3ff54
胡涛 <hutao1@ganji.com>
2015-07-09 22:19:54 +0800

    －Init Project－

---
+++ GJCFFileDownloadManager/GJCFFileDownloadManager/Demo/DemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  DemoViewController.h
+//  GJCFFileDownloadManager
+//
+//  Created by ZYVincent on 15/7/9.
+//  Copyright (c) 2015年 ZYProSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DemoViewController : UITableViewController
+
+@end
